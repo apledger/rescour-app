965e2dfa0263ad1f71e4afcca12b809674a10388
Alan Pledger <apledger3@gmail.com>
2013-11-01 12:57:17 -0400

    removed int reference in IE fix

--- src/core/market/thotpod.js
+++ src/core/market/thotpod.js
@@ -12,18 +12,7 @@
 
 var thotpod = (function () {
     var /** holds major version number for IE or NaN for real browsers */
-            msie              = int((/msie (\d+)/.exec(lowercase(navigator.userAgent)) || [])[1]),
-        jqLite,           // delay binding since jQuery could be loaded after us.
-        jQuery,           // delay binding
-        slice             = [].slice,
-        push              = [].push,
-        toString          = Object.prototype.toString,
-
-        /** @name angular */
-            angular           = window.angular || (window.angular = {}),
-        angularModule,
-        nodeName_,
-        uid               = ['0', '0', '0'];
+        toString          = Object.prototype.toString;
 
     function isArray(value) {
         return toString.apply(value) == '[object Array]';

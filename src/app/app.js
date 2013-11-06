db0dc6fccb97e28f11c62d500a598a203fdd304b
Alan Pledger <apledger3@gmail.com>
2013-11-06 11:30:09 -0500

    printing browser and version for dev

--- src/app/app.js
+++ src/app/app.js
@@ -27,7 +27,7 @@ angular.module('rescour.app',
             $httpProvider.defaults.withCredentials = true;
             $locationProvider.html5Mode(true);
 
-            console.log(BrowserDetectProvider);
+            console.log(BrowserDetectProvider.browser, BrowserDetectProvider.version);
             $routeProvider.when('/',
                 {
                     redirectTo: '/market/'
